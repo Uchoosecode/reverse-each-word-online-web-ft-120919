eaad83196e78ddd1f8aa5f5d9973c86bd706305a
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-14 01:22:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,8 +5,8 @@ def reverse_each_word(sentence1)
   array = sentence1.split(" ")
   
   scrambled_sentence = array.new 
-  array.collect do|sentence1| 
-    scrambled_sentence << sentence1.reverse #reverse each word in the array
+  array.collect do|sentence| 
+    scrambled_sentence << sentence.reverse #reverse each word in the array
   end
   return scrambled_sentence.join(" ")
 end
