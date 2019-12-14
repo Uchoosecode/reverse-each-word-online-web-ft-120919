c801ff015d943ca1bb5b26262b635ce3da4556e8
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-14 01:18:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,8 @@ require 'pry'
 def reverse_each_word(sentence1)
   array = []
     split_sent = sentence1.split 
-     puts array << split_sent.reverse
+     puts array << split_sent
+     puts array.reverse
 # binding.pry 
 end
 
