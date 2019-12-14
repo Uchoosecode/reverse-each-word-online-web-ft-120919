2b6d177b39b0c938cd2c56849c77860e559b2a7f
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-14 01:21:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,13 @@
 require 'pry'
 
 def reverse_each_word(sentence1)
-  array = sentence1.split(" ") #turn string into an array
-  test_array = []
-  array.collect do|sentence1| #iterate over the array
-    test_array << sentence1.reverse #reverse each word in the array
+  array = sentence1.split(" ")
+  
+  scrambled_sentence = array.new 
+  array.collect do|sentence1| 
+    scrambled_sentence << sentence1.reverse #reverse each word in the array
   end
-  test_array.join(" ")
+  return scrambled_sentence.join(" ")
 end
 
 
